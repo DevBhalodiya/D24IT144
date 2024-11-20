13ac95420a5306eb0ff9b199cf6f31eeaf25c11c
DevBhalodiya <113275734+DevBhalodiya@users.noreply.github.com>
2024-11-20 14:33:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// let menuicn = document.querySelector(".menuicn");
+// let nav = document.querySelector(".navcontainer");
+
+// menuicn.addEventListener("click", () => {
+//     nav.classList.toggle("navclose");
+// })
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
